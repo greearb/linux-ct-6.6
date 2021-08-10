49b1dc740326e2ef2d6796a5a10119b9e7400196
Ben Greear <greearb@candelatech.com>
2021-08-09 19:16:12 -0700

    wifi: Support disabling 160Mhz bandwidths in station mode.
    For testing purposes.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -3011,6 +3011,7 @@ struct cfg80211_assoc_link {
  *	Drivers shall disable MLO features for the current association if this
  *	flag is not set.
  * @ASSOC_REQ_DISABLE_TWT:  Disable TWT
+ * @ASSOC_REQ_DISABLE_160:  Disable 160Mhz
  */
 enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_HT			= BIT(0),
@@ -3021,6 +3022,7 @@ enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_EHT			= BIT(5),
 	CONNECT_REQ_MLO_SUPPORT			= BIT(6),
 	ASSOC_REQ_DISABLE_TWT                   = BIT(7),
+	ASSOC_REQ_DISABLE_160			= BIT(8),
 };
 
 /**
