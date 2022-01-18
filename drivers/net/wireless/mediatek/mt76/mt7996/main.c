a497595ade82cb48d7f606aab8f7a63547f11b13
Ben Greear <greearb@candelatech.com>
2022-01-17 17:19:36 -0800

    wifi: mt76: mt7996: Add tx_nss histogram to ethtool stats.
    To gain insight into how frames are being transmitted.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/main.c
@@ -1197,6 +1197,11 @@ static const char mt7996_gstrings_stats[][ETH_GSTRING_LEN] = {
 	"v_tx_mcs_12",
 	"v_tx_mcs_13",
 
+	"v_tx_nss_1",
+	"v_tx_nss_2",
+	"v_tx_nss_3",
+	"v_tx_nss_4",
+
 	/* per-vif rx counters */
 	"v_rx_nss1",
 	"v_rx_nss2",
